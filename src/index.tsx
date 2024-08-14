c12e8081f1a1bf6a6ce535911b34459782797f11
Quany <quany@live.com>
2024-08-14 23:47:05 +0800

    fixbuild

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,11 @@
-import React from "react";
-import { CopilotKit } from "@copilotkit/react-core";
-import { CopilotTextarea } from "@copilotkit/react-textarea";
+import React from 'react'
+import { CopilotKit } from '@copilotkit/react-core'
+import { CopilotTextarea } from '@copilotkit/react-textarea'
 
 export function iCodingTextArea(props: any) {
   return (
-    <CopilotKit
-      runtimeUrl={`https://agent.l0l.ink/api/copilotkit?apiKey=${props.apiKey}`}
-    >
+    <CopilotKit runtimeUrl={`https://agent.l0l.ink/api/copilotkit?apiKey=${props.apiKey}`}>
       <CopilotTextarea disableBranding={true} {...props} />
     </CopilotKit>
-  );
+  )
 }
