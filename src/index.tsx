fed1ae1f45e9a36b6342d106c048b47055569bfe
Quany <quany@live.com>
2024-08-14 23:05:31 +0800

    spec

---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import { CopilotKit } from "@copilotkit/react-core";
+import { CopilotTextarea } from "@copilotkit/react-textarea";
+
+export function iCodingTextArea(props: any) {
+  return (
+    <CopilotKit
+      runtimeUrl={`https://agent.l0l.ink/api/copilotkit?apiKey=${props.apiKey}`}
+    >
+      <CopilotTextarea disableBranding={true} {...props} />
+    </CopilotKit>
+  );
+}
