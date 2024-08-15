b62901d5ec217505238a1dd8ed155ba41df960ad
Quany <quany@live.com>
2024-08-15 17:08:47 +0800

    spec

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,17 @@
 import { CopilotKit } from '@copilotkit/react-core'
+import type { CopilotTextareaProps, HTMLCopilotTextAreaElement } from '@copilotkit/react-textarea'
 import { CopilotTextarea } from '@copilotkit/react-textarea'
 import React from 'react'
 
-export function iCodingTextArea(props: any) {
+export interface TextareaProps extends Omit<CopilotTextareaProps, 'copilotTextareaConfig'> {
+  apiKey: string
+}
+
+export const Textarea = React.forwardRef((props: TextareaProps, ref: React.Ref<HTMLCopilotTextAreaElement>) => {
   return (
     <CopilotKit runtimeUrl={`https://agent.l0l.ink/api/copilotkit?key=${props.apiKey}`}>
-      <CopilotTextarea disableBranding={true} {...props} />
+      <CopilotTextarea ref={ref} disableBranding={true} {...props} />
     </CopilotKit>
   )
-}
+})
+
